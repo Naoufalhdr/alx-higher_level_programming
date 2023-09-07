9fc9616238553f2339662557c09345f5f7c3753e
Naoufalhdr <naoufalhdr96@gmail.com>
2023-09-07 11:49:18 -0700

    6. Build my own calculator!

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,22 +5,18 @@ if __name__ == "__main__":
 
     # Check if the number of arguments is not 4
     if len(argv) != 4:
-        print("Usage:", argv[0], "<a> <operator> <b>")
+        print("Usage: {} <a> <operator> <b>".format(argv[0]))
         exit(1)
 
+    operators = {'+': add, '-': sub, '*': mul, '/': div}
     operator = argv[2]
     a = int(argv[1])
     b = int(argv[3])
 
     # Check if the operator is valid and perform the operation
-    if operator == '+':
-        print("{} + {} = {}".format(a, b, add(a, b)))
-    elif operator == '-':
-        print("{} - {} = {}".format(a, b, sub(a, b)))
-    elif operator == '*':
-        print("{} * {} = {}".format(a, b, mul(a, b)))
-    elif operator == '/':
-        print("{} / {} = {}".format(a, b, div(a, b)))
+    if operator in operators:
+        result = operators[operator](a, b)
+        print("{} {} {} = {}".format(a, operator, b, result))
     else:
         print("Unknown operator. Available operators: +, -, *, and /")
-        exit(1)
+
