312dcf3ca678d411c57544ad73991b830f9e741a
Naoufalhdr <naoufalhdr96@gmail.com>
2023-09-07 11:54:21 -0700

    6. Build my own calculator!

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,13 +2,17 @@
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     from sys import argv
+
     if len(argv) != 4:
         print("Usage: {} <a> <operator> <b>".format(argv[0]))
         exit(1)
+
     operators = {'+': add, '-': sub, '*': mul, '/': div}
-    a, operator, b = int(argv[1]), argv[2], int(argv[3])
-    if operator in operators:
-        result = operators[operator](a, b)
-        print("{} {} {} = {}".format(a, operator, b, result))
-    else:
-        print("Unknown operator. Available operators: +, -, *, and /")
+    operator = argv[2]
+    a = int(argv[1])
+    b = int(argv[3])
+
+    if operand not in arith:
+        print("Unknown operator. Only: +, -, * and / available")
+        exit(1)
+    print('{:d} {} {:d} = {:d}'.format(a, operand, b, arith[operand](a, b)))
