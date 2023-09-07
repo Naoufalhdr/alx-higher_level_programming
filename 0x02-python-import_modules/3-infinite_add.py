e8736774f130ed9eef321d855f4e6fda5a646d74
Naoufalhdr <naoufalhdr96@gmail.com>
2023-09-07 05:18:58 -0700

    3. Infinite addition

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    arguments = argv[1:]
+    result = 0
+    for arg in arguments:
+        result += int(arg)
+    print("{:d}".format(result))
