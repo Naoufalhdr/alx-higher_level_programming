4df46ce9e3c4900d47a881ece89c3b6e9e8dba48
Naoufalhdr <naoufalhdr96@gmail.com>
2023-09-13 08:51:40 -0700

    Fix check 4 error

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    new_matrix = list(map(lambda row: list(map(lambda x: x**2, row)), matrix))
+    new_matrix = list(
+            map(lambda row: list(map(lambda x: x**2, row)), matrix)
+            )
     return new_matrix
