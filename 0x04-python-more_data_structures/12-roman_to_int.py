ec857e13e3ff213f40998de6ee385ac1a4515221
Naoufalhdr <naoufalhdr96@gmail.com>
2023-09-13 06:52:17 -0700

    hot fix

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,6 +3,7 @@ def roman_to_int(roman_string):
     if (not isinstance(roman_string, str) or
             roman_string is None):
         return 0
+
     roman_dict = {
             'I': 1,
             'V': 5,
@@ -13,7 +14,10 @@ def roman_to_int(roman_string):
             'M': 1000
             }
     result = 0
+
     for roman_char in roman_string:
         if roman_char in roman_dict.keys():
             result += roman_dict[roman_char]
+        else:
+            return 0
     return result
