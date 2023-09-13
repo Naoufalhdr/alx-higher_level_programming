b1e717ea966a2a57036ed32afcade3e1b0610d35
Naoufalhdr <naoufalhdr96@gmail.com>
2023-09-13 03:30:21 -0700

    fix check 3 error

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,4 @@
 def square_matrix_simple(matrix=[]):
-    """
-    new_matrix = [[num**2 for num in row] for row in matrix]
-    return new_matrix
-    """
     new_matrix = []
     for row in matrix:
         new_row = list(map(lambda x: x**2, row))
