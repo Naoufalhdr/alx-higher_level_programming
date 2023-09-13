d92c49c7add09c025c57b39236d1313016d66d02
Naoufalhdr <naoufalhdr96@gmail.com>
2023-09-13 09:53:28 -0700

    16. CPython #1: PyBytesObject

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,17 @@
+#include "Python.h"
+
+/**
+  * print_python_list - Prints information about python lists objects
+  * @p: PyObject pointer to print info about
+  */
+void print_python_list(PyObject *p)
+{
+}
+
+/**
+  * print_python_bytes - Prints information about python bytes objects
+  * @p: PyObject pointer to print info about
+  */
+void print_python_bytes(PyObject *p)
+{
+}
