1aaf928670ea1f761742f4b6d58a44c087e1b7d3
Naoufalhdr <naoufalhdr96@gmail.com>
2023-09-13 04:16:51 -0700

    Add a function that adds all unique integers in a list (only once for each integer).

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = []
-    for num in my_list:
-        if num == search:
-            num = replace
-        new_list.append(num)
+    """ replaces all occurrences of an element by another in a new list """
+    new_list = list(map(lambda x: replace if x == search else x, my_list))
     return new_list
