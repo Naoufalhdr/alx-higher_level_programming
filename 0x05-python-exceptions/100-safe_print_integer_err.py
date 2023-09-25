a168834dd203603736d436fa84dc8a79c859451c
Naoufal hdr <naoufalhdr@gmail.com>
2023-09-25 20:53:04 +0100

    fixing: check 5 error

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
+
 import sys
 
 def safe_print_integer_err(value):
         try:
             print("{:d}".format(value))
-            return True
+            return (True)
         except (ValueError, TypeError) as e:
             print("Exception:", e, file=sys.stderr)
-            return False
+            return (False)
