223449c54d0a61b97bc7964a2b923dbfe84e8e32
Naoufal hdr <naoufalhdr@gmail.com>
2023-10-04 20:10:03 +0100

    Task 6: fix Check 3 Module is not documented

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,7 +1,30 @@
 #!/usr/bin/python3
+"""This module defines a function for matrix multiplication"""
 
 
 def matrix_mul(m_a, m_b):
+    """
+    Multiply two matrices.
+    The function takes two matrices represented as lists of lists and returns
+    their product as a list of lists. It also performs input validation and
+    raises appropriate errors for invalid input or incompatible matrices.
+
+    Args:
+        m_a (list of lists): The first matrix represented as a list of lists.
+        m_b (list of lists): The second matrix represented as a list of lists.
+
+    Returns:
+        list of lists: The result of matrix multiplication as a list of lists.
+
+    Raises:
+        Raise a TypeError:
+        - If either m_a or m_b is not a list or if they are not lists of lists
+        - If any element in m_a or m_b is not an integer or a float.
+        - If the rows in m_a or m_b are not of the same size.
+        - If m_a and m_b cannot be multiplied due to incompatible dimensions.
+        Raise a ValueError:
+        - If either m_a or m_b is empty or contains empty rows.
+    """
 
     # Check if m_a or m_b is not a list
     if not isinstance(m_a, list):
