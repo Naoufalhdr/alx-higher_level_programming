4c5ee51854b449b939dfac094c5b9635db72f206
Naoufal hdr <naoufalhdr@gmail.com>
2023-10-04 20:01:56 +0100

    Task 6: fix Check 3 Module is not documented

--- 0x07-python-test_driven_development/6-max_integer.py
+++ 0x07-python-test_driven_development/6-max_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Module to find the max integer in a list"""
+"""Module to find the max integer in a list
+"""
 
 
 def max_integer(list=[]):
@@ -7,11 +8,10 @@ def max_integer(list=[]):
     Find and return the maximum integer in a list of integers.
 
     Args:
-    lst (list of int): A list of integers to search for the maximum value.
+        lst (list of int): A list of integers to search for the maximum value.
 
     Returns:
-    int : The maximum integer in the list.
-    None : if the list is empty.
+        int : The maximum integer in the list, or None if the list is empty.
     """
     if len(list) == 0:
         return None
