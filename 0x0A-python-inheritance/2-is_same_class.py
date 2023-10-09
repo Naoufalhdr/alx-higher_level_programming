7a3241a253e5c10a3fcaa27e527fc7c869090e31
Naoufal hdr <naoufalhdr@gmail.com>
2023-10-09 22:30:58 +0100

    Task 2: Fix check 3 error

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
+"""Defines a class-checking function"""
 
 
 def is_same_class(obj, a_class):
     """
     Check if an object is exactly an instance of the specified class.
     """
-    if type(obj) is a_class:
-        return True
-    return False
+    return type(obj) is a_class:
