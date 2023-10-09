789a3c0f6167e7dddb8e6d39ec29f8464af74306
Naoufal hdr <naoufalhdr@gmail.com>
2023-10-09 21:52:45 +0100

    Task 0: fix check 4 error

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -5,13 +5,5 @@
 def lookup(obj):
     """
     Return a list of attributes and methods associated with the given object.
-
-    Parameters:
-    obj (object): The Python object whose attributes and methods are to be
-                  listed.
-
-    Returns:
-    list: A list containing the names of attributes and methods associated
-          with the object.
     """
     return (dir(obj))
