535e28fbee628604ac9eae6f022ce4dd7f4972ed
Naoufal hdr <naoufalhdr@gmail.com>
2023-10-09 23:55:27 +0100

    Task 8: Rectangle

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,25 +1,10 @@
 #!/usr/bin/python3
 """Module to define class BaseGeometry"""
-
-
-class BaseGeometry:
-    """Class BaseGeometry"""
-
-    def area(self):
-        """Placeholder method for calculating the area of a geometric shape.
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validate that a value is a positive integer."""
-        if type(value) is not int:
-            raise TypeError("{:s} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{:s} must be greater than 0".format(name))
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Class Rectangle"""
+    """Class Rectangle; inherits from BaseGeometry"""
 
     def __init__(self, width, height):
         self.integer_validator("width", width)
