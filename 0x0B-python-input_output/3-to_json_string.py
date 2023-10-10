adea672c8fdbf97f3e2f61447194dac179b6dc16
Naoufal hdr <naoufalhdr@gmail.com>
2023-10-11 00:12:45 +0100

    Task 3: fixing pycodestyle

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -13,4 +13,4 @@ def to_json_string(my_obj):
     Returns:
         str: The JSON representation of the object as a string.
     """
-    return json.dumps(my_obj)    
+    return json.dumps(my_obj)
