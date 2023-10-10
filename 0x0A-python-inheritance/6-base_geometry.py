ce640fd5b5776b463a09f589ea40bab140ec5cd2
Naoufal hdr <naoufalhdr@gmail.com>
2023-10-10 20:32:47 +0100

    Task 6: fix type error

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -9,4 +9,4 @@ class BaseGeometry:
         Raises an Exception indicating that the area() method is not
         implemented.
         """
-        raise Exception("area() is not implemented.")
+        raise Exception("area() is not implemented")
