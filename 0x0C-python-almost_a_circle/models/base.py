8ec31b10583a9f065ab3ba73697e477e082a79f5
Naoufal hdr <naoufalhdr@gmail.com>
2023-10-14 05:57:31 +0100

    Task 15: Update the class Base by adding the static method def to_json_string(...): that returns the JSON string representation of list_dictionaries

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-"""
-This module defines the Base class.
-The Base class manages unique IDs for instances of derived classes.
-"""
+"""This module defines the Base class."""
+import json
 
 
 class Base:
-    """Base class"""
+    """The Base class manages unique IDs for instances of derived classes."""
     __nb_objects = 0
 
     def __init__(self, id=None):
@@ -16,3 +14,15 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        Returns the JSON string representation of a list of dictionaries.
+
+        Args:
+            list_dictionaries (list): A list of dictionaries.
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return []
+        return json.dumps(list_dictionaries)
