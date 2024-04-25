d660706f61e8323aa456f085e31db426d23cda8f
Naoufal hdr <naoufalhdr@gmail.com>
2024-04-25 15:13:34 +0100

    Task 0: update the code for pydocodestyle

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-
+""" Lists all states from the database hbtn_0e_0_usa  """
 import sys
 import MySQLdb
 
-arg = sys.argv
-conn = MySQLdb.connect(host="localhost", user=arg[1], passwd=arg[2], db=arg[3])
 
-cursor = conn.cursor()
-
-cursor.execute("SELECT * FROM states")
-
-results = cursor.fetchall()
-
-for row in results:
-    print(row)
-
-cursor.close()
-conn.close()
+if __name__ == "__main__":
+    arg = sys.argv
+    conn = MySQLdb.connect(host="localhost", user=arg[1],
+                           passwd=arg[2], db=arg[3], port=3306)
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM states")
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
+    cursor.close()
+    conn.close()
