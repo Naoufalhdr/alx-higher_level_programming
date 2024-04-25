3afbf0712632a5a0f765bc70a6eba345cc490ecf
Naoufal hdr <naoufalhdr@gmail.com>
2024-04-25 17:26:50 +0100

    Taks 2: Filter states by user input

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Lists all values where name matches the argument from the database
+hbtn_0e_0_usa """
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    arg = sys.argv
+    conn = MySQLdb.connect(host="localhost", user=arg[1],
+                           passwd=arg[2], db=arg[3], port=3306)
+    cursor = conn.cursor()
+    cursor.execute(f"SELECT * FROM states WHERE name = '{arg[4]}'")
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
+    cursor.close()
+    conn.close()
