18e6354767adaad61354bf42eba11c1d87bfcd62
Naoufal hdr <naoufalhdr@gmail.com>
2024-04-28 09:20:48 +0100

    Task 12: Update a state

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Modify the nem of the State object from the database """
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    instance = session.query(State).filter_by(id=2).first()
+    instance.name = 'New Mexico'
+    session.commit()
+    session.close()
