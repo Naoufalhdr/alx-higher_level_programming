766ef61f2c008fc1073cf4e0b10b003443d35990
Naoufal hdr <naoufalhdr@gmail.com>
2024-05-10 15:34:44 +0100

    Task 2: fix check 7 error

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,12 +2,13 @@
 """  takes in a URL and an email, sends a POST request to the passed URL with
 the email as a parameter, and displays the body of the response """
 from sys import argv
-from urllib import request
+from urllib import request, parse
 
 
 if __name__ == "__main__":
     url = argv[1]
-    email = argv[2].encode('utf-8')
-    req = request.Request(url, email)
+    value = {"email": argv[2]}
+    data = parse.urlencode(value).encode("ascii")
+    req = request.Request(url, data)
     with request.urlopen(req) as response:
-        print(response.read().decode('utf-8'))
+        print(response.read().decode("utf-8"))
