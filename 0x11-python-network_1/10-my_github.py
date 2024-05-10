d30f723a26af14f2dc03737b8d643ff46012b641
Naoufal hdr <naoufalhdr@gmail.com>
2024-05-10 22:55:27 +0100

    Task 9: My GitHub! fix check 6 error

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,11 +6,10 @@ Usage: ./10-my_github.py <GitHub username> <GitHub password>
 """
 from sys import argv
 import requests
+from requests.auth import HTTPBasicAuth
 
 
 if __name__ == "__main__":
-    url = "https://api.github.com/user"
-    auth_header = {"Authorization": f"Basic {argv[1]}:{argv[2]}"}
-
-    response = requests.get(url, headers=auth_header)
-    print(response.json().get("id"))
+    auth_header = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    r = requests.get("https://api.github.com/user", auth=auth_header)
+    print(r.json().get("id"))
