e0eddbcb02b49baa0992421365f0f803c19d0734
Naoufal hdr <naoufalhdr@gmail.com>
2024-05-10 19:02:38 +0100

    Task 8: test

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,15 +7,13 @@ import requests
 
 if __name__ == "__main__":
     url = "http://0.0.0.0:5000/search_user"
-    if len(argv) >= 2:
-        r = requests.get(url, data={'q': argv[1]})
-        try:
-            json_data = r.json()
-            if json_data:
-                print(f"[{json_data.get('id')}] {json_data.get('name')}")
-            else:
-                print("No result")
-        except requests.exceptions.JSONDecodeError as json_err:
-            print("Not a valid JSON")
-    else:
-        print("No result")
+    letter = "" if len(argv) == 1 else argv[1]
+    r = requests.get(url, data={'q': letter})
+    try:
+        json_data = r.json()
+        if json_data:
+            print(f"[{json_data.get('id')}] {json_data.get('name')}")
+        else:
+            print("No result")
+    except requests.exceptions.JSONDecodeError as json_err:
+        print("Not a valid JSON")
