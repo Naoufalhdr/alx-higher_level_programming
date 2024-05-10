4c0b5f6089b4f3d011204139837b019e018e9be1
Naoufal hdr <naoufalhdr@gmail.com>
2024-05-10 18:55:33 +0100

    Task 8: Search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" This script takes in a letter and sends a POST request to a URL with the
+letter as a parameter. """
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    if len(argv) >= 2:
+        r = requests.get(url, data={'q': argv[1]})
+        try:
+            json_data = r.json()
+            if json_data:
+                print(json_data.get(argv[1]))
+            else:
+                print("No result")
+        except requests.exceptions.JSONDecodeError as json_err:
+            print("Not a valid JSON")
+    else:
+        print("No result")
