109b4a01192095a531106e7799610ada1946bad7
Naoufalhdr <naoufalhdr96@gmail.com>
2024-06-04 21:47:39 +0100

    Task 3: Stars wars movie title

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}/`;
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error('error:', error);
+  } else if (response.statusCode !== 200) {
+    console.log('status code:', response.statusCode);
+  } else {
+    const data = JSON.parse(body);
+    console.log(data.title);
+  }
+});
